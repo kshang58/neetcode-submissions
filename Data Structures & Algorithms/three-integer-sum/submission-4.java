fde1f2bd04a04f93d7b19e23b40fe8cf1348b451
kshang58 <78334367+kshang58@users.noreply.github.com>
2026-07-25 18:48:14 -0400

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        // -4 -1 -1 0 1 2
+        List<List<Integer>> rslt = new ArrayList<>();
+        for (int i = 0; i < nums.length; i ++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int target = 0 - nums[i];
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    j ++;
+                    continue;
+                }
+                if (nums[j] + nums[k] == target) {
+                    List<Integer> sol = new ArrayList<>();
+                    sol.add(nums[i]);
+                    sol.add(nums[j]);
+                    sol.add(nums[k]);
+                    rslt.add(sol);
+                    j ++;
+                    k --;
+                } else if (nums[j] + nums[k] < target) {
+                    j ++;
+                } else {
+                    k --;
+                }
+            }
+
+        }
+        return rslt;
+    }
+}

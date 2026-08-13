8ce79c37d50803db9add669dd93a6d05ad225697
kshang58 <78334367+kshang58@users.noreply.github.com>
2026-08-13 19:45:02 -0400

    Add: length-of-last-word - submission-1

---
+++ Data Structures & Algorithms/length-of-last-word/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length();
+        int i = n - 1;
+        int length = 0;
+        while (s.charAt(i) == ' ') {
+            i --;
+        }
+        while (i >= 0 && s.charAt(i) != ' ') {
+            i --;
+            length ++;
+        }
+        return length;
+    }
+}

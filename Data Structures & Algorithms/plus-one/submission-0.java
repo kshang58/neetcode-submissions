e6cd51d45c3bcc4c079a7f1388c2d40659cddaf5
kshang58 <78334367+kshang58@users.noreply.github.com>
2026-08-24 13:40:57 -0400

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length - 1;
+        while (n >= 0) {
+            if (digits[n] != 9) {
+                digits[n] ++;
+                return digits;
+            } else {
+                digits[n] = 0;
+                n --;
+            }
+        }
+        int[] rslt = new int[digits.length + 1];
+        rslt[0] = 1;
+        return rslt;
+    }
+}
